50b869587d65bc86b9fff77c1043a1d51e954677
Tyrrance <tyrrancem37@gmail.com>
2023-04-22 21:36:42 -0400

    Set up this section to show all employees.

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ class employeeDB {
         this.connection = connection;
 
     }
-
+    // Set up this section to show all employees
     allEmployees() {
         return this.connection.promise().query(
             "SELECT employee.id, employee.first_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
